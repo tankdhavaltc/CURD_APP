ba8d2e7c4bc6748100e85db096b9f8e39a148ea3
tankdhavaltc <tankdhaval2001@gmail.com>
2023-02-02 14:25:48 +0530

    fix issue

--- src/Actions/User.jsx
+++ src/Actions/User.jsx
@@ -2,22 +2,7 @@ import axios from "axios";
 import { ADD_USER_FAIL, ADD_USER_REQUEST, ADD_USER_SUCCESS, ALL_USERS_FAIL, ALL_USERS_REQUEST, ALL_USERS_SUCCESS, CLEAR_ERRORS, CLEAR_MESSAGE, DELETE_USER_FAIL, DELETE_USER_REQUEST, DELETE_USER_SUCCESS, UPDATE_USER_FAIL, UPDATE_USER_REQUEST, UPDATE_USER_SUCCESS, USER_DETAILS_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS } from "../constants/userConstants";
 
 // const API = process.env.REACT_APP_API_LINK;
-const API = "https://crudcrud.com/api/f757a53d019b4232a74e147e38d2b4a6";
-
-
-// clear error
-export const clearErrors = () => async (dispatch) => {
-    dispatch({
-        type: CLEAR_ERRORS,
-    });
-};
-
-// clear message
-export const clearMessage = () => async (dispatch) => {
-    dispatch({
-        type: CLEAR_MESSAGE,
-    });
-};
+const API = "https://crudcrud.com/api/f757a53d019b4232a74e147e38d2b4a6/users";
 
 // delete user
 export const deleteUser = (id) => async (dispatch) => {
@@ -26,13 +11,13 @@ export const deleteUser = (id) => async (dispatch) => {
 
         const config = { headers: { "Content-Type": "application/json" } };
 
-         await axios.delete(`${API}/users/${id}`, config);
+        await axios.delete(`${API}/${id}`, config);
 
         dispatch({ type: DELETE_USER_SUCCESS, payload: id });
     } catch (error) {
         dispatch({
             type: DELETE_USER_FAIL,
-            payload: error.response.data.message,
+            payload: "Something went wrong.",
         });
     }
 };
@@ -54,7 +39,7 @@ export const getAllUsers = () => async (dispatch) => {
     } catch (error) {
         dispatch({
             type: ALL_USERS_FAIL,
-            payload: error.response.data.message,
+            payload: "Something went wrong.",
         });
     }
 };
@@ -73,7 +58,7 @@ export const addUser = (userData) => async (dispatch) => {
     } catch (error) {
         dispatch({
             type: ADD_USER_FAIL,
-            payload: error.response.data.message,
+            payload: "Something went wrong.",
         });
     }
 }
@@ -90,7 +75,7 @@ export const editUser = (id, user) => async (dispatch) => {
     } catch (error) {
         dispatch({
             type: UPDATE_USER_FAIL,
-            payload: error.response.data.message,
+            payload: "Something went wrong.",
         });
     }
 }
@@ -106,7 +91,7 @@ export const getUserDetails = (id) => async (dispatch) => {
     } catch (error) {
         dispatch({
             type: USER_DETAILS_FAIL,
-            payload: error.response.data.message,
+            payload: "Something went wrong.",
         });
     }
 };
